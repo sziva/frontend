93e67c00d9ea71568fa7c65bd0c6edfce8f11973
sziva <zs3732@student.uni-lj.si>
2020-01-09 10:46:58 +0100

    kinda working

--- src/app_client/controllers/imagesController.js
+++ src/app_client/controllers/imagesController.js
@@ -9,53 +9,54 @@
             {
                 id: 1,
                 url: 'http://www.skuner.si/wp-content/uploads/2015/02/umetniske0062.jpg',
-                likes: 4
-                comments = [
+                like: [
+                    {
+                        id: 1,
+                        like: 1
+                    }
+                ],
+                comment: [
                     {
                         id: 1,
                         comment: 'Odlićna fotografija'
-                    },
-                    {
-                        id: 2,
-                        comment: 'Odlićna fotografija je zelo lepa'       
-                    },
+                    }
                 ]
             },
             {
                 id: 2,
                 url: 'http://www.osnovnasolasostro.si/ucenci/13-14/9a_Perkovic_Luka/www/slike/VESOLJE3.jpg',
-                likes: 6
+                like: [
+                    {
+                        id: 1,
+                        like: 100
+                    }
+                ]
             },
             {
                 id: 3,
                 url: 'https://vesolje.net/navtika/novice/2009/02/ngc2818.jpg',
-                likes: 5
-                comment = [
+                like:[
+                    {
+                        id: 1,
+                        like: 78
+                    }
+                ],
+                comment: [
                     {
                         id: 1,
                         comment: 'Odlićna fotografija RES'
+                    },
+                    {
+                        id: 2,
+                        comment: 'Odlićna fotografija'
+                    },
+                    {
+                        id: 3,
+                        comment: 'Vesolje'
                     }
                 ]
             }
         ];
-
-        // vm.likes = [
-        //     {
-        //         id: 1,
-        //         id_image: 1,
-        //         like: 3
-        //     {
-        //         id: 2,
-        //         id_image: 2,
-        //         like: 15    
-        //     },
-        //     {
-        //         id: 3,
-        //         id_image: 2,
-        //         like: 4
-        //     }
-        // ];
-        
         // ODKOMENTIRI!!!!
         // vm.images = {}
         // vm.getImages = function() {
@@ -76,53 +77,53 @@
 
         // vm.getImages();
 
-        vm.newComment = function(){
-            var error = false;
-            vm.inputError = "";
-            vm.formError = "";
+        // vm.newComment = function(){
+        //     var error = false;
+        //     vm.inputError = "";
+        //     vm.formError = "";
 
-            if(!vm.todo.description){
-                vm.inputError = "Dodaj komentar!";
-                error = true;
-            }
+        //     if(!vm.todo.description){
+        //         vm.inputError = "Dodaj komentar!";
+        //         error = true;
+        //     }
 
-            if (error) {
-                vm.formError = "Komentar ni vnešen!";
-                return false;
-            } else {
-                vm.createComment();
-            }
-        }
+        //     if (error) {
+        //         vm.formError = "Komentar ni vnešen!";
+        //         return false;
+        //     } else {
+        //         vm.createComment();
+        //     }
+        // }
 
-        vm.createComment = function() {
-            vm.image.insertion_date = new Date();
-            imagesService.postComment(vm.id, vm.text).then(
-                function success(response) {
-                    vm.message = "Uspešno objavljen komentar.";
-                    Swal.fire({
-                        text: vm.message,
-                        type: 'success',
-                        showConfirmButton: false,
-                        timer: 1000
-                    });
-                    vm.showComments();
-                    vm.comments = {};
-                    vm.commentsList.push(response);
-                    vm.closeNew();
-                } , function error(response) {
-                    vm.message = "Prišlo je do napake!";
-                    Swal.fire({
-                        text: vm.message,
-                        type: 'error',
-                        showConfirmButton: false,
-                        timer: 1000
-                    });
-                }
-            );  
-        }
+        // vm.createComment = function() {
+        //     vm.image.insertion_date = new Date();
+        //     imagesService.postComment(vm.id, vm.text).then(
+        //         function success(response) {
+        //             vm.message = "Uspešno objavljen komentar.";
+        //             Swal.fire({
+        //                 text: vm.message,
+        //                 type: 'success',
+        //                 showConfirmButton: false,
+        //                 timer: 1000
+        //             });
+        //             vm.showComments();
+        //             vm.comments = {};
+        //             vm.commentsList.push(response);
+        //             vm.closeNew();
+        //         } , function error(response) {
+        //             vm.message = "Prišlo je do napake!";
+        //             Swal.fire({
+        //                 text: vm.message,
+        //                 type: 'error',
+        //                 showConfirmButton: false,
+        //                 timer: 1000
+        //             });
+        //         }
+        //     );  
+        // }
        
         vm.showComment = function(comment){
-            vm.currentComment = comment;
+            vm.currentImage.comment = comment;
             $('#showImageCommentModal').modal("show");
         }
 
@@ -141,34 +142,34 @@
         }
 
         vm.showLike = function(like){
-            vm.currentLike = like;
+            vm.currentImage.like = like;
             $('#showImageCommentModal').modal("show");
         }
 
-        vm.updateLike = function(){
-          imagesService.putLike(vm.id, vm.like).then(
-              function success(response) {
-                  vm.message = "Like je zabeležen.";
-                  Swal.fire({
-                      text: vm.message,
-                      type: 'success',
-                      showConfirmButton: false,
-                      timer: 1000
-                  });
-                  vm.closeNew();
-                  vm.showLike();
-                  vm.currentLike = like + 1;
-              }, function error(response) {
-                  vm.message = "Prišlo je do napake!";
-                  Swal.fire({
-                      text: vm.message,
-                      type: 'error',
-                      showConfirmButton: false,
-                      timer: 1000
-                  });
-              }
-          );
-        }    
+        // vm.updateLike = function(){
+        //   imagesService.putLike(vm.id, vm.like).then(
+        //       function success(response) {
+        //           vm.message = "Like je zabeležen.";
+        //           Swal.fire({
+        //               text: vm.message,
+        //               type: 'success',
+        //               showConfirmButton: false,
+        //               timer: 1000
+        //           });
+        //           vm.closeNew();
+        //           vm.showLike();
+        //           vm.currentLike = like + 1;
+        //       }, function error(response) {
+        //           vm.message = "Prišlo je do napake!";
+        //           Swal.fire({
+        //               text: vm.message,
+        //               type: 'error',
+        //               showConfirmButton: false,
+        //               timer: 1000
+        //           });
+        //       }
+        //   );
+        // }    
 
          // vm.showComments = function(){
         //     $('#showImageModal').modal("show");
@@ -186,93 +187,6 @@
         //     $('#updateTodoModal').modal("show");
         // }
 
-        // vm.showTodoList = function(){
-    //           todoService.getTodoList(vm.idUser).then(
-    //               function success(response) {
-    //                   vm.todoList = response.data;
-    //               }, function error(response) {
-    //                   vm.message = "Prišlo je do napake!";
-    //                   Swal.fire({
-    //                       text: vm.message,
-    //                       type: 'error',
-    //                       showConfirmButton: false,
-    //                       timer: 1000
-    //                   });
-    //               }
-    //           );
-    //       }
-
-    //      vm.updateTodo = function(){
-    //           vm.currentTodo.insertion_date = new Date();
-    //           todoService.putTodo(vm.idUser, vm.currentTodo).then(
-    //               function success(response) {
-    //                   vm.message = "Uspešno posodobljen opis.";
-    //                   Swal.fire({
-    //                       text: vm.message,
-    //                       type: 'success',
-    //                       showConfirmButton: false,
-    //                       timer: 1000
-    //                   });
-    //                   vm.closeUpdate();
-    //                   vm.showTodoList();
-    //                   vm.currentTodo = {};
-    //               }, function error(response) {
-    //                   vm.message = "Prišlo je do napake!";
-    //                   Swal.fire({
-    //                       text: vm.message,
-    //                       type: 'error',
-    //                       showConfirmButton: false,
-    //                       timer: 1000
-    //                   });
-    //               }
-    //           );
-    //       }       
-
-    //       vm.newTodo = function(){
-    //           var error = false;
-    //           vm.inputError = "";
-    //           vm.formError = "";
-
-    //           if(!vm.todo.description){
-    //               vm.inputError = "Vnesite opis!";
-    //               error = true;
-    //           }
-
-    //           if (error) {
-    //               vm.formError = "Vpis ni vnešen!";
-
-    //               return false;
-    //           } else {
-    //               vm.createTodo();
-    //           }
-    //       }
-
-    //       vm.createTodo = function() {
-    //           vm.todo.insertion_date = new Date();
-    //           todoService.postTodo(vm.idUser, vm.todo).then(
-    //               function success(response) {
-    //                   vm.message = "Uspešno naložena slika.";
-    //                   Swal.fire({
-    //                       text: vm.message,
-    //                       type: 'success',
-    //                       showConfirmButton: false,
-    //                       timer: 1000
-    //                   });
-    //                   vm.showTodoList();
-    //                   vm.todo = {};
-    //                   vm.todoList.push(response);
-    //                   vm.closeNew();
-    //               }, function error(response) {
-    //                   vm.message = "Prišlo je do napake!";
-    //                   Swal.fire({
-    //                       text: vm.message,
-    //                       type: 'error',
-    //                       showConfirmButton: false,
-    //                       timer: 1000
-    //                   });
-    //               }
-    //           );  
-    //       }
 
         
 
